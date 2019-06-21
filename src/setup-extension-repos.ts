b4ec4bab5c4717aacb0ce8b47c43fb3ce7129d0a
Logan Ramos <lramos15@gmail.com>
2019-06-21 09:14:42 -0700

    More windows support

---
+++ src/setup-extension-repos.ts
@@ -0,0 +1,9 @@
+import * as os from 'os';
+import * as cp from 'child_process';
+
+// Execute the bat file if it's windows else the sh file
+if (os.platform() === "win32") {
+    cp.execSync('./setup-extension-repos.bat');
+} else {
+    cp.execSync('./setup-extension-repos.sh');
+}
