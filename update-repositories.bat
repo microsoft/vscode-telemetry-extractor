db1b26c01bc6d90a44e9b88fd621e25e31453e10
Logan Ramos <lramos15@gmail.com>
2019-06-21 09:08:21 -0700

    More windows scripts

---
+++ update-repositories.bat
@@ -0,0 +1,14 @@
+cd src/telemetry-sources
+cd TypeScript && git pull && cd ..
+cd vscode && git pull && cd ..
+cd vscode-azure-account && git pull && cd ..
+cd vscode-chrome-debug && git pull && cd ..
+cd vscode-chrome-debug-core && git pull && cd ..
+cd vscode-docker && git pull && cd ..
+cd vscode-extension-telemetry && git pull && cd ..
+cd vscode-go && git pull && cd ..
+cd vscode-html-languageservice && git pull && cd ..
+cd vscode-json-languageservice && git pull && cd ..
+cd vscode-mono-debug && git pull && cd ..
+cd vscode-node-debug && git pull && cd ..
+cd vscode-node-debug2 && git pull && cd ..
