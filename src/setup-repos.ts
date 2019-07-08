a24c0eff5a8095fc2f5ef70e1f16b56a0e560314
Logan Ramos <lramos15@gmail.com>
2019-07-08 11:56:59 -0700

    Remove VS Code specific items

--- src/setup-repos.ts
+++
@@ -1,9 +0,0 @@
-import * as os from 'os';
-import * as cp from 'child_process';
-
-// Execute the bat file if it's windows else the sh file
-if (os.platform() === "win32") {
-    cp.execSync('setup-repositories.bat');
-} else {
-    cp.execSync('./setup-repositories.sh');
-}
