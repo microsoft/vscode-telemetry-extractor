01cd8d2fe5749ee36e2a52a0e4d3ac80693322b2
Logan Ramos <lramos15@gmail.com>
2019-07-16 09:22:19 -0700

    Help command updates

--- src/cli-options.ts
+++ src/cli-options.ts
@@ -5,7 +5,7 @@ import * as path from 'path';
 
 function resolveDirectories(options: commandLineArgs.CommandLineOptions): commandLineArgs.CommandLineOptions {
     const cwd = process.cwd();
-    
+
     if (options.sourceDir) {
         options.sourceDir = (options.sourceDir as string[]).map(d => path.resolve(cwd, d));
     }
@@ -17,13 +17,13 @@ function resolveDirectories(options: commandLineArgs.CommandLineOptions): comman
 }
 
 export const optionDefinitions = [
-    { name: 'sourceDir', alias: 's', description: 'The folder which you want to extract telemetry from', type: String, multiple: true},
-    { name: 'excludedDir', alias: 'x', description: 'A subdirectory which you would like to exclude from the extraction', type: String, multiple: true, defaultValue: [] },
-    { name: 'config', alias: 'c', description: 'A JSON Configuration file containing extraction details', type: String},
+    { name: 'sourceDir', alias: 's', description: 'The folder which you want to extract telemetry from (relative to the CWD)', type: String, multiple: true },
+    { name: 'excludedDir', alias: 'x', description: 'A subdirectory which you would like to exclude from the extraction (relative to the CWD)', type: String, multiple: true, defaultValue: [] },
+    { name: 'config', alias: 'c', description: 'A JSON Configuration file containing extraction details', type: String },
     { name: 'outputDir', alias: 'o', description: 'The directory which you would like the outputted JSON file to be placed in', type: String },
-    { name: 'eventPrefix', alias: 'p', type: String, description: 'The string you wish to prepend to every telemetry event.', defaultValue: ''},
-    { name: 'help', alias: 'h', type: Boolean, description: 'Displays the help dialog which provides more information on how to use the tool', defaultValue: false},
-    { name: 'applyEndpoints', alias: 'e', type: Boolean, defaultValue: false}
+    { name: 'eventPrefix', alias: 'p', type: String, description: 'The string you wish to prepend to every telemetry event.', defaultValue: '' },
+    { name: 'help', alias: 'h', type: Boolean, description: 'Displays the help dialog which provides more information on how to use the tool', defaultValue: false },
+    { name: 'applyEndpoints', alias: 'e', type: Boolean, defaultValue: false }
 ];
 
 export interface ParserOptions {
@@ -33,4 +33,4 @@ export interface ParserOptions {
     lowerCaseEvents: boolean;
 }
 
-export const options = resolveDirectories(commandLineArgs(optionDefinitions, { partial: true}));
+export const options = resolveDirectories(commandLineArgs(optionDefinitions, { partial: true }));
