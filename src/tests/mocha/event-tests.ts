edb64d35fb5667b2ffb8e5badd7aa7613dbb4176
Jonathan Yi <101259035+jonathanjyi@users.noreply.github.com>
2024-12-11 16:44:32 +0000

    added additional unit tests for cs properties

--- src/tests/mocha/event-tests.ts
+++ src/tests/mocha/event-tests.ts
@@ -10,6 +10,7 @@ import { getResolvedDeclaration } from "../../lib/save-declarations";
 import { Property } from "../../lib/common-properties";
 import { patchDebugEvents } from "../../lib/debug-patch";
 import { ParserOptions } from "../../lib/source-spec";
+import { Metadata } from "../../lib/events";
 
 const sourceDir = path.join(cwd(), 'src/tests/mocha/resources/source');
 const excludedDirs = [path.join(sourceDir, 'excluded')];
@@ -88,5 +89,13 @@ describe('Resolve Tests', () => {
         assert.deepStrictEqual(e1Properties[2], new Property('property_EOneP3', 'SystemMetaData', 'FeatureInsight', '1.57.0', "lramos15", "Test event", 'none'));
         assert.deepStrictEqual(e1Properties[3], new Property('measurement_EOneM1', 'SystemMetaData', 'FeatureInsight', undefined, undefined, undefined, 'none', true));
         assert.deepStrictEqual(e1Properties[4], new Property('measurement_EOneM<NUMBER>', 'SystemMetaData', 'FeatureInsight', undefined, undefined, undefined, 'none', true));
-    });
+        const testCSEOneProperties = declarations.events.dataPoints[4].properties;
+        assert.deepStrictEqual(testCSEOneProperties[0], new Metadata('owner', 'jonathanyi'));
+        assert.deepStrictEqual(testCSEOneProperties[1], new Metadata('comment', 'Output to Console log test event.'));
+        assert.deepStrictEqual(testCSEOneProperties[2], new Property('property_ECSharpP1', 'SystemMetaData', 'FeatureInsight', undefined, undefined, undefined, "NA"));
+        assert.deepStrictEqual(testCSEOneProperties[3], new Property('property_ECSharpP2', 'CallstackOrException', 'PerformanceAndHealth', undefined, undefined, undefined, "NA"));
+        assert.deepStrictEqual(testCSEOneProperties[4], new Property('property_ECSharpP3', 'SystemMetaData', 'FeatureInsight', "1.57.0", "jonathanjyi", "Test event", "none", undefined));
+        assert.deepStrictEqual(testCSEOneProperties[5], new Property('measurement_ECSharpM1', 'SystemMetaData', 'FeatureInsight', undefined, undefined, undefined, 'none', true));
+        assert.deepStrictEqual(testCSEOneProperties[6], new Property('measurement_ECSharpM<NUMBER>', 'SystemMetaData', 'FeatureInsight', undefined, undefined, undefined, 'none', true));
+    })
 });
--- src/tests/mocha/resources/source/file4.cs
+++ src/tests/mocha/resources/source/file4.cs
@@ -7,10 +7,10 @@ public class Program
     {
       /* __GDPR__
         "TestCSEOne" : {
-          "owner": "jonathanjyi",
+          "owner": "jonathanyi",
           "comment": "Output to Console log test event.",
-          "property_ECSharpP1": { "classification": "SystemMetaData", "purpose": "FeatureInsight" },
-          "property_ECSharpP2": { "classification": "CallstackOrException", "purpose": "PerformanceAndHealth" },
+          "property_ECSharpP1": { "classification": "SystemMetaData", "purpose": "FeatureInsight", "endpoint": "NA" },
+          "property_ECSharpP2": { "classification": "CallstackOrException", "purpose": "PerformanceAndHealth", "endpoint": "NA" },
           "property_ECSharpP3": { "classification": "SystemMetaData", "purpose": "FeatureInsight", "expiration": "1.57.0", "owner": "jonathanjyi", "comment": "Test event" },
           "measurement_ECSharpM1": { "classification": "SystemMetaData", "purpose": "FeatureInsight", "isMeasurement": true },
           "measurement_ECSharpM<NUMBER>": { "classification": "SystemMetaData", "purpose": "FeatureInsight", "isMeasurement": true }
