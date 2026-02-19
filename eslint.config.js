d49786989ef1bdb8a0d94c5441b5b87d6f2742ab
João Moreno <joaomoreno@users.noreply.github.com>
2026-02-19 13:46:16 +0100

    fix: update ESLint configuration to ignore specific test resources

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,9 @@ const tseslint = require('typescript-eslint');
 
 
 module.exports = tseslint.config(
+  {
+    ignores: ['src/tests/mocha/resources/**'],
+  },
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
 );
